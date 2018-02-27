711d4fc3a796ac6d68381020d7311f6f4f1859b9
Jonathan Finn <jonathanfill11@gmail.com>
2018-02-27 14:27:49 -0600

    Fixing my original mistake. Visuals sould be in line with original design now

--- src/ui.js
+++ src/ui.js
@@ -10,14 +10,21 @@ let ui = {
         arm: document.getElementById('gyro-arm'),
         number: document.getElementById('gyro-number')
     },
-    robotDiagram: {
-        arm: document.getElementById('robot-arm')
+    wheel: {
+        flwheel: document.getElementById('fl-wheel'),
+        frwheel: document.getElementById('fr-wheel'),
+        blwheel: document.getElementById('bl-wheel'),
+        brwheel: document.getElementById('br-wheel')
     },
-    example: {
-        button: document.getElementById('example-button'),
-        readout: document.getElementById('example-readout').firstChild
+    pad: {
+        blpad: document.getElementById('bottomleft_pad')
+        brpad: document.getElementById('bottomright_pad')
+        mlpad: document.getElementById('midleft_pad')
+        mrpad: document.getElementById('midright_pad')
+        tlpad: document.getElementById('topleft_pad')
+        trpad: document.getElementById('topright_pad')
     },
-    autoSelect: document.getElementById('auto-select'),
+    colorselect: document.getElementById('color-select')
     armPosition: document.getElementById('arm-position')
 };
 
@@ -35,28 +42,60 @@ let updateGyro = (key, value) => {
     ui.gyro.number.innerHTML = ui.gyro.visualVal + 'º';
 };
 NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', updateGyro);
+// -----------------------------------------------------------------------------------------
+// EXAMPLE code
 
 // The following case is an example, for a robot with an arm at the front.
+// NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
+//     // 0 is all the way back, 1200 is 45 degrees forward. We don't want it going past that.
+//     if (value > 1140) {
+//         value = 1140;
+//     }
+//     else if (value < 0) {
+//         value = 0;
+//     }
+//     // Calculate visual rotation of arm
+//     var armAngle = value * 3 / 20 - 45;
+//     // Rotate the arm in diagram to match real arm
+//     ui.robotDiagram.arm.style.transform = `rotate(${armAngle}deg)`;
+// });
+
+// -------------------------------------------------------------------------------------------------------------
+// FIX THIS
+
 NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // 0 is all the way back, 1200 is 45 degrees forward. We don't want it going past that.
-    if (value > 1140) {
-        value = 1140;
-    }
-    else if (value < 0) {
-        value = 0;
-    }
-    // Calculate visual rotation of arm
-    var armAngle = value * 3 / 20 - 45;
-    // Rotate the arm in diagram to match real arm
-    ui.robotDiagram.arm.style.transform = `rotate(${armAngle}deg)`;
+    // Calculate visual rotation of wheels
+    var wheelAngle = value * 3 / 20 - 45;
+    // Rotate the wheels in diagram to match real arm
+    ui.wheel.flwheel.style.transform = `rotate(${wheelAngle}deg)`;
+});
+NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
+    // Calculate visual rotation of wheels
+    var wheelAngle = value * 3 / 20 - 45;
+    // Rotate the wheels in diagram to match real arm
+    ui.wheel.frwheel.style.transform = `rotate(${wheelAngle}deg)`;
+});
+NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
+    // Calculate visual rotation of wheels
+    var wheelAngle = value * 3 / 20 - 45;
+    // Rotate the wheels in diagram to match real arm
+    ui.wheel.blwheel.style.transform = `rotate(${wheelAngle}deg)`;
+});
+NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
+    // Calculate visual rotation of wheels
+    var wheelAngle = value * 3 / 20 - 45;
+    // Rotate the wheels in diagram to match real arm
+    ui.wheel.brwheel.style.transform = `rotate(${wheelAngle}deg)`;
 });
 
+// ----------------------------------------------------------------------------------------------------------------
+
 // This button is just an example of triggering an event on the robot by clicking a button.
-NetworkTables.addKeyListener('/SmartDashboard/example_variable', (key, value) => {
-    // Set class active if value is true and unset it if it is false
-    ui.example.button.classList.toggle('active', value);
-    ui.example.readout.data = 'Value is ' + value;
-});
+// NetworkTables.addKeyListener('/SmartDashboard/example_variable', (key, value) => {
+//     // Set class active if value is true and unset it if it is false
+//     ui.example.button.classList.toggle('active', value);
+//     ui.example.readout.data = 'Value is ' + value;
+// });
 
 NetworkTables.addKeyListener('/robot/time', (key, value) => {
     // This is an example of how a dashboard could display the remaining time in a match.
@@ -65,31 +104,31 @@ NetworkTables.addKeyListener('/robot/time', (key, value) => {
 });
 
 // Load list of prewritten autonomous modes
-NetworkTables.addKeyListener('/SmartDashboard/autonomous/modes', (key, value) => {
-    // Clear previous list
-    while (ui.autoSelect.firstChild) {
-        ui.autoSelect.removeChild(ui.autoSelect.firstChild);
-    }
-    // Make an option for each autonomous mode and put it in the selector
-    for (let i = 0; i < value.length; i++) {
-        var option = document.createElement('option');
-        option.appendChild(document.createTextNode(value[i]));
-        ui.autoSelect.appendChild(option);
-    }
-    // Set value to the already-selected mode. If there is none, nothing will happen.
-    ui.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
-});
+// NetworkTables.addKeyListener('/SmartDashboard/autonomous/modes', (key, value) => {
+//     // Clear previous list
+//     while (ui.autoSelect.firstChild) {
+//         ui.autoSelect.removeChild(ui.autoSelect.firstChild);
+//     }
+//     // Make an option for each autonomous mode and put it in the selector
+//     for (let i = 0; i < value.length; i++) {
+//         var option = document.createElement('option');
+//         option.appendChild(document.createTextNode(value[i]));
+//         ui.autoSelect.appendChild(option);
+//     }
+//     // Set value to the already-selected mode. If there is none, nothing will happen.
+//     ui.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
+// });
 
 // Load list of prewritten autonomous modes
-NetworkTables.addKeyListener('/SmartDashboard/autonomous/selected', (key, value) => {
-    ui.autoSelect.value = value;
-});
+// NetworkTables.addKeyListener('/SmartDashboard/autonomous/selected', (key, value) => {
+//     ui.autoSelect.value = value;
+// });
 
 // The rest of the doc is listeners for UI elements being clicked on
-ui.example.button.onclick = function() {
-    // Set NetworkTables values to the opposite of whether button has active class.
-    NetworkTables.putValue('/SmartDashboard/example_variable', this.className != 'active');
-};
+// ui.example.button.onclick = function() {
+//     // Set NetworkTables values to the opposite of whether button has active class.
+//     NetworkTables.putValue('/SmartDashboard/example_variable', this.className != 'active');
+// };
 // Reset gyro value to 0 on click
 ui.gyro.container.onclick = function() {
     // Store previous gyro val, will now be subtracted from val for callibration
@@ -98,13 +137,13 @@ ui.gyro.container.onclick = function() {
     updateGyro('/SmartDashboard/drive/navx/yaw', ui.gyro.val);
 };
 // Update NetworkTables when autonomous selector is changed
-ui.autoSelect.onchange = function() {
-    NetworkTables.putValue('/SmartDashboard/autonomous/selected', this.value);
-};
-// Get value of arm height slider when it's adjusted
-ui.armPosition.oninput = function() {
-    NetworkTables.putValue('/SmartDashboard/arm/encoder', parseInt(this.value));
-};
+// ui.autoSelect.onchange = function() {
+//     NetworkTables.putValue('/SmartDashboard/autonomous/selected', this.value);
+// };
+// // Get value of arm height slider when it's adjusted
+// ui.armPosition.oninput = function() {
+//     NetworkTables.putValue('/SmartDashboard/arm/encoder', parseInt(this.value));
+// };
 
 addEventListener('error',(ev)=>{
     ipc.send('windowError',{mesg:ev.message,file:ev.filename,lineNumber:ev.lineno})
