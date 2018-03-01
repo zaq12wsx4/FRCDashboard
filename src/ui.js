105313a44a2e3a2a5bf98e2c94144c97e66b7413
Jonathan Finn <jonathanfill11@gmail.com>
2018-03-01 16:10:04 -0600

    Made some of the javascript link with the robot

--- src/ui.js
+++ src/ui.js
@@ -17,19 +17,23 @@ let ui = {
         brwheel: document.getElementById('br-wheel')
     },
     pad: {
-        blpad: document.getElementById('bottomleft_pad')
-        brpad: document.getElementById('bottomright_pad')
-        mlpad: document.getElementById('midleft_pad')
-        mrpad: document.getElementById('midright_pad')
-        tlpad: document.getElementById('topleft_pad')
+        blpad: document.getElementById('bottomleft_pad'),
+        brpad: document.getElementById('bottomright_pad'),
+        mlpad: document.getElementById('midleft_pad'),
+        mrpad: document.getElementById('midright_pad'),
+        tlpad: document.getElementById('topleft_pad'),
         trpad: document.getElementById('topright_pad')
     },
-    colorselect: document.getElementById('color-select')
-    armPosition: document.getElementById('arm-position')
+    colorselect: document.getElementById('color-select'),
+    armPosition: document.getElementById('arm-position'),
+    positionselect: document.getElementById('position-select')
 };
 
 // Key Listeners
 
+NetworkTables.addKeyListener('/SmartDashboard/autonomous state', (key, value) => {
+    ui.robotState = 'Robot is ' + value;
+});
 // Gyro rotation
 let updateGyro = (key, value) => {
     ui.gyro.val = value;
@@ -41,7 +45,7 @@ let updateGyro = (key, value) => {
     ui.gyro.arm.style.transform = `rotate(${ui.gyro.visualVal}deg)`;
     ui.gyro.number.innerHTML = ui.gyro.visualVal + 'º';
 };
-NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', updateGyro);
+NetworkTables.addKeyListener('/SmartDashboard/Heading', updateGyro);
 // -----------------------------------------------------------------------------------------
 // EXAMPLE code
 
@@ -63,29 +67,17 @@ NetworkTables.addKeyListener('/SmartDashboard/drive/navx/yaw', updateGyro);
 // -------------------------------------------------------------------------------------------------------------
 // FIX THIS
 
-NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // Calculate visual rotation of wheels
-    var wheelAngle = value * 3 / 20 - 45;
-    // Rotate the wheels in diagram to match real arm
-    ui.wheel.flwheel.style.transform = `rotate(${wheelAngle}deg)`;
-});
-NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // Calculate visual rotation of wheels
-    var wheelAngle = value * 3 / 20 - 45;
-    // Rotate the wheels in diagram to match real arm
+NetworkTables.addKeyListener('/SmartDashboard/Front Right Position', (key, value) => {
     ui.wheel.frwheel.style.transform = `rotate(${wheelAngle}deg)`;
 });
-NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // Calculate visual rotation of wheels
-    var wheelAngle = value * 3 / 20 - 45;
-    // Rotate the wheels in diagram to match real arm
-    ui.wheel.blwheel.style.transform = `rotate(${wheelAngle}deg)`;
+NetworkTables.addKeyListener('/SmartDashboard/Front Left Position', (key, value) => {
+    ui.wheel.flwheel.style.transform = `rotate(${wheelAngle}deg)`;
 });
-NetworkTables.addKeyListener('/SmartDashboard/arm/encoder', (key, value) => {
-    // Calculate visual rotation of wheels
-    var wheelAngle = value * 3 / 20 - 45;
-    // Rotate the wheels in diagram to match real arm
+NetworkTables.addKeyListener('/SmartDashboard/Back Right Position', (key, value) => {
     ui.wheel.brwheel.style.transform = `rotate(${wheelAngle}deg)`;
+});
+NetworkTables.addKeyListener('/SmartDashboard/Back Left Position', (key, value) => {
+    ui.wheel.blwheel.style.transform = `rotate(${wheelAngle}deg)`;
 });
 
 // ----------------------------------------------------------------------------------------------------------------
@@ -102,6 +94,24 @@ NetworkTables.addKeyListener('/robot/time', (key, value) => {
     // We assume here that value is an integer representing the number of seconds left.
     ui.timer.innerHTML = value < 0 ? '0:00' : Math.floor(value / 60) + ':' + (value % 60 < 10 ? '0' : '') + value % 60;
 });
+
+// ----------------------------------------------------------------------------------------------------------------
+// When game starts, wait for field code
+//   When recieve the field setup string (ex. lrr) {
+//     str alliance =  get alliance color
+//     str setup = get field setup
+//     front
+//     for (i = 0, i < setup[].length, i++) {
+//       var temp_letter = setup.charAt(i)
+//       if (i = 0) {
+//         if (temp_letter = 'l') {
+//           ui.pad.blpad.animate().attr({ fill: '#f03' })
+//       }
+//     }
+//   }
+// }
+
+
 
 // Load list of prewritten autonomous modes
 // NetworkTables.addKeyListener('/SmartDashboard/autonomous/modes', (key, value) => {
@@ -135,6 +145,9 @@ ui.gyro.container.onclick = function() {
     ui.gyro.offset = ui.gyro.val;
     // Trigger the gyro to recalculate value.
     updateGyro('/SmartDashboard/drive/navx/yaw', ui.gyro.val);
+};
+ui.positionselect.onchange = function() {
+    NetworkTables.putValue('/SmartDashboard/Robot Starting Position', this.value);
 };
 // Update NetworkTables when autonomous selector is changed
 // ui.autoSelect.onchange = function() {
